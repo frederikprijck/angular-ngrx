8bfbf2dfddb6ead2c227b9347419d679a4782f7a
Frederik Prijck <frederik.prijck@gmail.com>
2019-12-14 23:35:54 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from "@angular/core";
-import { ShowsService } from "./shows/shows.service";
-import { Subject } from "rxjs";
-
-import { switchMap, startWith, map } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
@@ -11,25 +7,4 @@ import { switchMap, startWith, map } from "rxjs/operators";
 })
 export class AppComponent {
   title = "thisdot-ngrx";
-  refreshShows$ = new Subject();
-  shows$ = this.refreshShows$.pipe(
-    startWith(null),
-    switchMap(() => this.showsService.getAll())
-  );
-
-  favoriteShows$ = this.shows$.pipe(map(shows => shows.filter(show => show.isFavorite)));
-
-  constructor(private showsService: ShowsService) {}
-
-  favoriteShow(showId) {
-    this.showsService
-      .favoriteShow(showId)
-      .subscribe(() => this.refreshShows$.next());
-  }
-
-  unfavoriteShow(showId) {
-    this.showsService
-      .unfavoriteShow(showId)
-      .subscribe(() => this.refreshShows$.next());
-  }
 }
